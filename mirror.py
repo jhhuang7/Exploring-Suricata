04bd762f37c4d3d7895fde6f945307b3096288b1
Harry Huang <40063626+jhhuang7@users.noreply.github.com>
2020-09-17 15:52:02 +1000

    Added mesh topo build

--- mirror.py
+++ mirror.py
@@ -1,11 +1,11 @@
 from mininet.cli import CLI
 from mininet.net import Mininet
-from mininet.link import Link,TCLink, Intf
+from mininet.link import Link, TCLink, Intf
 from mininet.node import RemoteController, Controller
 
 
 if '__main__' == __name__:
-	net = Mininet(link = TCLink)
+	net = Mininet(link=TCLink)
 	
 	topos = ["simple", "mesh", "tree"]
 	mode = 0 # Change this ranging from 0 to 2
@@ -20,10 +20,27 @@ if '__main__' == __name__:
 		net.addLink(h1, s1)
 		net.addLink(h2, s1)
 		net.addLink(h3, s1)
-	"""
+		
 	elif topo == "mesh":
-		# Set up mesh topology
-	"""
+		h1 = net.addHost('h1')
+		h2 = net.addHost('h2')
+		h3 = net.addHost('h3')
+		s1 = net.addSwitch('s1')
+		s2 = net.addSwitch('s2')
+		s3 = net.addSwitch('s3')
+		s4 = net.addSwitch('s4')
+		
+		net.addLink(s2, s1)
+		net.addLink(s3, s1)
+		net.addLink(s4, s1)
+		net.addLink(s3, s2)
+		net.addLink(s4, s2)
+		net.addLink(s3, s4)
+		net.addLink(h1, s1)
+		net.addLink(h1, s1)
+		net.addLink(h2, s3)
+		net.addLink(h3, s1)
+		
 	elif topo == "tree":
 		h1 = net.addHost('h1')
 		h2 = net.addHost('h2')
@@ -43,10 +60,9 @@ if '__main__' == __name__:
 		net.addLink(h3, s1)
 	
 	c0 = net.addController('c0', controller=Controller)
-	Intf('eth1', node =h3)
+	Intf('eth1', node=h3)
 	net.build()
 	c0.start()
 	s1.start([c0])
 	CLI(net)
 	net.stop();
-
