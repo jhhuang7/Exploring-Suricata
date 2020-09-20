e619abedc9373242bc69bbca3f37d2372ef8741f
Harry Huang <40063626+jhhuang7@users.noreply.github.com>
2020-09-20 11:45:07 +1000

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -7,9 +7,12 @@ from mininet.node import RemoteController, Controller
 if '__main__' == __name__:
 	net = Mininet(link=TCLink)
 	
-	topos = ["simple", "mesh", "tree"]
-	mode = 2  # Change this ranging from 0 to 2
+	topos = ["simple", "mesh", "tree"]  # May add most hosts and switches for more complexity for each topo
+	mode = 0  # Change this ranging from 0 to 2
 	topo = topos[mode]
+	
+	hosts = 0
+	switches = 0
 	
 	if topo == "simple":
 		h1 = net.addHost('h1')
@@ -20,6 +23,9 @@ if '__main__' == __name__:
 		net.addLink(h1, s1)
 		net.addLink(h2, s1)
 		net.addLink(h3, s1)
+		
+		hosts = 3
+		switches = 1
 		
 	elif topo == "mesh":
 		h1 = net.addHost('h1')
@@ -41,6 +47,9 @@ if '__main__' == __name__:
 		net.addLink(h2, s3)
 		net.addLink(h3, s1)
 		
+		hosts = 3
+		switches = 4
+		
 	elif topo == "tree":
 		h1 = net.addHost('h1')
 		h2 = net.addHost('h2')
@@ -58,12 +67,22 @@ if '__main__' == __name__:
 		net.addLink(h1, s3)
 		net.addLink(h2, s5)
 		net.addLink(h3, s1)
+		
+		hosts = 3
+		switches = 5
 	
 	c0 = net.addController('c0', controller=Controller)
 	Intf('eth1', node=h3)  # May need to change 'eth' depending on the VM's internet connection
 	net.build()
 	c0.start()
+	
 	s1.start([c0])
+	if switches >= 4:
+		s2.start([c0])
+		s3.start([c0])
+		s4.start([c0])
+	if switches >= 5:
+		s5.start([c0])
+	
 	CLI(net)
 	net.stop();
-
