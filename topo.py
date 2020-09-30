5c4f0e220610eb6b15629ed814e234b2581a2b69
jhhuang7 <juhua.huang@uqconnect.edu.au>
2020-09-30 13:36:37 +1000

    Custom tree.

--- topo.py
+++ topo.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from mininet.cli import CLI
 from mininet.net import Mininet
 from mininet.link import Link, TCLink, Intf
@@ -9,20 +10,45 @@ Script to build a custom tree topology in mininet given number of hosts.
 """
 
 def build_tree(num_hosts: int):
-    net = Mininet(link=TCLink)
+    """
+    Build up the tree network topology with hosts, switches and links
+    based on the given number of hosts. Tree is going to be a binary tree.
+    """
 
-    switches = []
+    net = Mininet(link=TCLink)
+    topo = [None]  # Element at i = 0 doesn't get used
+
+    level = math.ceil(math.log2(num_hosts))  # level of tree hosts are on
+    num_switches = int(sum([math.pow(2, n) for n in range(0, level)]))
     
-    # Build up the tree network topology with hosts, switches and links
-    # based on the given number of hosts. Tree is going to be a binary tree.
+    # Add switches
+    for i in range(0, num_switches):
+        name = "s" + str(i + 1)
+        switch = net.addSwitch(name)
+        topo.append(switch)
+
+    # Add hosts
+    for j in range(0, num_hosts):
+        name = "h" + str(j + 1)
+        host = net.addHost(name)
+        topo.append(host)
+    
+    # Add links
+    for k in range(1, len(topo)):
+        # Left child
+        if 2 * k <= len(topo) - 1:
+            net.addLink(topo[k], topo[2 * k])
+        
+        # Right child
+        if 2 * k + 1 <= len(topo) - 1:
+            net.addLink(topo[k], topo[2 * k + 1])
 
     c0 = net.addController("c0", controller=Controller)
-	
-    for switch in switches:
-        switch.start([c0])
+    for s in range(1, num_switches + 1):
+        topo[s].start([c0])
     
-    s1.cmd("ovs-vsctl del-port s1-eth2")
-    s1.cmd("ovs-vsctl add-port s1 s1-eth2 -- --id=@p get port s1-eth2 -- \
+    topo[1].cmd("ovs-vsctl del-port s1-eth2")
+    topo[1].cmd("ovs-vsctl add-port s1 s1-eth2 -- --id=@p get port s1-eth2 -- \
         --id=@m create mirror name=m0 select-all=true output-port=@p -- \
         set bridge s1 mirrors=@m")
     
@@ -42,6 +68,10 @@ def main(args):
         num_hosts = int(args[0])
     except ValueError:
         print("Number of hosts must be an int.")
+        return
+    
+    if num_hosts < 3:
+        print("Number of hosts must be greater or equal to 3.")
         return
 
     build_tree(num_hosts)
