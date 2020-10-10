42afc32ade2be9a151c68c934f5b0b1c4fd3c183
COMS4200 <kau23@live.com>
2020-10-10 13:38:16 +1000

    Reflect Spoof protection

--- output.py
+++ output.py
@@ -27,10 +27,18 @@ dosProtection = False
 wlist = {"10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5","10.0.0.6"}
 blist = {}
 hardTimeOut = 60;
-ingressPort = 2;
+ingressPort = {'s1' : [1, 3]};
+outFacingSwitches = ['s1'];
 
 
+def reflectedSpoofProtection():
+	for switch in outFacingSwitches:
+		for port in ingressPort[switch]:
+			for key in wlist:
+				a = "sudo ovs-ofctl add-flow "+switch+" hard_timeout=0,in_port="+str(port)+",dl_type=0x0800,nw_src="+str(key)+",actions=drop"
+				os.system(a)
 
+reflectedSpoofProtection()
 while True:
 	line = conn.recv(1000).decode("utf-8")
 	try:
@@ -54,78 +62,21 @@ while True:
 
 					if srcIP not in wlist :
 						os.system("sudo ovs-ofctl del-flows s1")
-
+						reflectedSpoofProtection()
 						blist[srcIP] = date
 						for key in blist:
 							os.system("sudo ovs-ofctl add-flow s1 hard_timeout=60,dl_type=0x0800,nw_src="+str(key)+",actions=drop")
 						
-						print('------------------------')
-						print('Current Flow Table for s1')
-						os.system("sudo ovs-ofctl dump-flows s1")
+						# print('------------------------')
+						# print('Current Flow Table for s1')
+						# os.system("sudo ovs-ofctl dump-flows s1")
 					
 
 			except Exception as e:
 				continue
-			# try:
-
-			# except:
-			# 	print("HERE!")
- 			# srcIP = str(parsed['src_ip'])
-			# if srcIP not in blist:
-			# 	os.system("sudo ovs-ofctl del-flows s1")
-			# 	blist[srcIP] = True
-				
-			# 	print("Readding" + blist)
-			# 	for key in blist:
-			# 		os.system("sudo ovs-ofctl add-flow s1 priority=65535,hard_timeout=300,nw_src="+str(srcIP)+",actions=drop")
-
-				
-			
-		# if (parsed['proto'] == u'ICMP' and parsed["src_ip"]=="10.0.0.1"):
-		# 	if line == "proto":
-		# 		continue
-		# 	else:
-		# 		a = datetime.datetime.strptime(parsed["timestamp"], "%Y-%m-%dT%H:%M:%S.%f%z")
-		# 		if lastA == None:
-		# 			lastA = a
-		# 			continue
-		# 		else:
-		# 			diff =  a - lastA
-		# 			print("Difference is")
-		# 			print(diff.total_seconds())
-		# 			lastA = a
-
-		# 			if (diff.total_seconds() < 1):
-		# 				if not dosProtection:						
-		# 					print("Adding flow")
-
-		# 					os.system("sudo ovs-ofctl add-flow s1 priority=65535,hard_timeout=120,nw_src=10.0.0.2,actions=drop")
-
-
 	except Exception as e:
 		print(e)
-		#UNCOMMENT TO PRINT ERRORS
 		continue
 
 sock.close()
 
-# # a = sock.recv(100)
-# # print(a)
-
-
-# while True:
-# 	where = file1.tell()
-# 	line = file1.readline()
-
-# 	if not line:
-# 		file1.seek(where)
-		
-# 	else:
-# 		try:
-# 			parsed = json.loads(line)
-
-
-# 		except:
-# 			continue	
-		
-	
