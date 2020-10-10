c7ed4e1dad44d85acfbf7d7af32f9b7e1cb21ef7
jhhuang7 <juhua.huang@uqconnect.edu.au>
2020-10-10 14:21:17 +1000

    Up to speed

--- output.py
+++ output.py
@@ -20,65 +20,46 @@ hardTimeOut = 20
 installed = False
 
 while True:
-	line = conn.recv(10000).decode("utf-8")
-	# {'timestamp': '2020-10-09T11:18:06.861731+1000', 'flow_id': 2144724435, 'in_iface': 
-	# 'h3-eth0', 'event_type': 'alert', 'src_ip': '10.0.0.4', 'src_port': 8081, 
-	# 'dest_ip': '10.0.0.1', 'dest_port': 53757, 'proto': 'TCP', 
-	# 'alert': {'action': 'allowed', 'gid': 1, 'signature_id': 0, 'rev': 0, 'signature': 'Ports Scanned', 'category': '', 'severity': 3}}
+	line = conn.recv(1000).decode("utf-8")
+	
 	try:
 		split = line.split('\n')
 
 		for i in split:
-			if (i == ''):
-				continue
-			# print(i)
-			parsed = json.loads(i)
-
-
-			if (parsed['proto'] == 'TCP' and parsed['alert']['signature'] =='Ports Scanned'):
-				pass
-				# srcIP = parsed['src_ip']
-			# 	print(parsed["timestamp"])
-				# date = datetime.datetime.strptime(parsed["timestamp"], "%Y-%m-%dT%H:%M:%S.%f%z")
-				# if (not installed):
-				# 	print("GETTING HERE!")
-				# 	os.system("sudo ovs-ofctl del-flows s1")
-				# 	os.system("sudo ovs-ofctl add-flow s1 dl_type=0x0800,nw_src=10.0.0.1,actions=drop")
-				# 	os.system("sudo ovs-ofctl add-flow s1 dl_type=0x0800,nw_src=10.0.0.4,actions=drop")
-				# 	installed = True
-
-				# if (srcIP in blist):
-				# 	diff = date - blist[srcIP]
-
-				# 	if diff.total_seconds() >= hardTimeOut:
-				# 		del blist[srcIP]
-				# 	else:
-				# 		continue
-
-				# 	if srcIP not in wlist:
-
-				# 		blist[srcIP] = date
-				# 		for key in blist:
-						
-				# 		print('------------------------')
-				# 		print('Current Flow Table for s1')
-				# 		os.system("sudo ovs-ofctl dump-flows s1")
-			# print(parsed)
-			# sort out the logic
+			try:
+				if (i == ''):
+					continue
+				parsed = json.loads(i)
 			
-		# if parsed['tcp']['rst'] >= 200:  # If we've gotten more than 200 TCP RST packets
-		# 	print("!!!!!!!!!!!!!!!!!!!!!!!!!!" + parsed)
-		# 	srcIP = parsed['src_ip']
-		# 	print(parsed["timestamp"])
-		# 	date = datetime.datetime.strptime(parsed["timestamp"], "%Y-%m-%dT%H:%M:%S.%f%z")
-					
-		# 
+			
+				if (parsed['proto'] == u'TCP' and parsed['alert']['signature'] =='Ports Scanned'):
+					srcIP = parsed['src_ip']
+				
+					date = datetime.datetime.strptime(parsed["timestamp"], "%Y-%m-%dT%H:%M:%S.%f%z")
+				
+
+					if (srcIP in blist):
+					 	diff = date - blist[srcIP]
+					 	if diff.total_seconds() >= hardTimeOut:
+					 		del blist[srcIP]
+					 	else:
+					 		continue
+
+				 	if srcIP not in wlist:
+						os.system("sudo ovs-ofctl del-flows s1")
+				 		blist[srcIP] = date
+				 		
+				
+				 		os.system("sudo ovs-ofctl add-flow s1 hard_timeout=20,dl_type=0x0800,nw_src="+srcIP+",actions=drop")
+			
+				
+			except Exception as e:
+				continue
+			
+		
 					
 	except Exception as e:
 		print("Error:", e)
-		if ('data' in str(e)):
-			print("------------------ ERROR ----------------------------")
-			print(line)
 		continue
 
 sock.close()
