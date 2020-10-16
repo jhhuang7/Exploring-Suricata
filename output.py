4e3e21600f7abfac7a6c7191b8deea807a5dc09b
COMS4200 <kau23@live.com>
2020-10-16 23:05:52 +1000

    Updates

--- output.py
+++ output.py
@@ -24,24 +24,27 @@ dosProtection = False
 # os.system("sudo ovs-ofctl del-flows s1 nw_src="+sourceIp)
 # os.system("sudo ovs-ofctl add-flow s1 priority=65535,hard_timeout=300,nw_src="+sourceIp+",actions=drop")
 
-wlist = {"10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5","10.0.0.6"}
 blist = {}
 hardTimeOut = 60;
+
+internalIPs = {"10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5","10.0.0.6"}
 ingressPort = {'s1' : [1, 3]};
 outFacingSwitches = ['s1'];
-
 
 def reflectedSpoofProtection():
 	for switch in outFacingSwitches:
 		for port in ingressPort[switch]:
-			for key in wlist:
-				a = "sudo ovs-ofctl add-flow "+switch+" hard_timeout=0,in_port="+str(port)+",dl_type=0x0800,nw_src="+str(key)+",actions=drop"
-				os.system(a)
+			for key in internalIPs:
+				cmd = "sudo ovs-ofctl add-flow " + switch + 
+						" hard_timeout=0,priority=65535,in_port="+str(port)+
+						",dl_type=0x0800,nw_src="+str(key)+",actions=drop"
+				os.system(cmd)
 
-reflectedSpoofProtection()
-hardTimeOut = 60
+# reflectedSpoofProtection()
+
+hardTimeOut = 10
 while True:
-	line = conn.recv(1000).decode("utf-8")
+	line = conn.recv(512).decode("utf-8")
 	try:
 		lines =  line.split('\n')
 		for i in lines:
@@ -50,9 +53,8 @@ while True:
 					continue
 				parsed = json.loads(i)
 
-				if (parsed['proto'] == u'ICMP') or parsed['proto'] == u'TCP':
+				if (str(parsed['proto']) == 'ICMP') or (parsed['proto'] == u'TCP' and int(str(parsed['alert']['signature_id'])) ==1):
 					srcIP = parsed['src_ip']
-					print(parsed["timestamp"])
 					date = datetime.datetime.strptime(parsed["timestamp"], "%Y-%m-%dT%H:%M:%S.%f%z")
 					if (srcIP in blist) :
 						diff = date - blist[srcIP]
@@ -63,10 +65,10 @@ while True:
 
 					if srcIP not in wlist :
 						os.system("sudo ovs-ofctl del-flows s1")
-						reflectedSpoofProtection()
+						# reflectedSpoofProtection()
 						blist[srcIP] = date
 						for key in blist:
-							os.system("sudo ovs-ofctl add-flow s1 hard_timeout=60,dl_type=0x0800,nw_src="+str(key)+",actions=drop")
+							os.system("sudo ovs-ofctl add-flow s1 hard_timeout="+str(hardTimeOut)+",dl_type=0x0800,nw_src="+str(key)+",actions=drop")
 						
 						# print('------------------------')
 						# print('Current Flow Table for s1')
@@ -74,6 +76,7 @@ while True:
 					
 
 			except Exception as e:
+				# print(e)
 				continue
 	except Exception as e:
 		print(e)
