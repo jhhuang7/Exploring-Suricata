5e63719091219215dc54de2e9322acd31ccacdef
jhhuang7 <juhua.huang@uqconnect.edu.au>
2020-10-18 16:35:03 +1000

    IPS mode

--- cmds.sh
+++ cmds.sh
@@ -1,2 +1,2 @@
-suricata -c ../suricata-3.1/suricata.yaml -i hs-eth0
-rm /home/sdn/portmirroring/*.sock
+suricata -c ../suricata-3.1/suricata.yaml -i h3-eth0
+rm /home/sdn/Exploring-Suricata/*.sock
---
+++ cmds2.sh
@@ -0,0 +1,2 @@
+suricata -c ../suricata-3.1/suricata.yaml -q 0
+rm /home/sdn/Exploring-Suricata/*.sock
