3f0f71534f9a2601f68e53c9d49e6c9e255cd65a
COMS4200 <kau23@live.com>
2020-10-19 21:39:05 +1000

    Fixed reflected spoof integration

--- output.py
+++ output.py
@@ -10,11 +10,13 @@ os.system("sudo ovs-ofctl del-flows s1")
 os.system("rm eve.sock")
 os.system("pkill -f suricata") # Remove all instances of suricata
 
-
+'''
+ONOS Mode works off an ONOS controller
+CLI Mode (where ONOS = False) is more crude and just uses os library and ovs - ofctl to manages installation
+'''
 onosMode = False
 anomalyMode = False
-median = 3
-stdDev = 1
+
 
 hardTimeOut = 10
 internalIPs = {"10.0.0.2", "10.0.0.3", "10.0.0.4", "10.0.0.5", "10.0.0.6"}
@@ -26,18 +28,15 @@ if onosMode:
     auth = ('onos', 'rocks')
 else:
     suricataInterfaces = {'h3-eth0':  's1', 'h8-eth0': 's5'} 
-    ingressPort = {'s1': [1, 3], 's5' : [2]};
-
+    ingressPort = {'s1': [1], 's5' : [2]};
 
 def reflectedSpoofProtection():
     for switch in ingressPort:
         for port in ingressPort[switch]:
             for key in internalIPs:
-                cmd = "sudo ovs-ofctl add-flow " + switch + \
-                        " hard_timeout=0,priority=65535,in_port=" + str(port) +\
-                        ",dl_type=0x0800,nw_src=" + str(key) + ",actions=drop"
+                print(f"{switch} {port} {key}")
+                cmd = "sudo ovs-ofctl add-flow " + switch + " hard_timeout=0,priority=65535,in_port="+str(port)+",dl_type=0x0800,nw_src="+str(key)+",actions=drop"
                 os.system(cmd)
-
 
 def reflectedSpoofProtectionONOS():
     pay_load_to_post = {
@@ -78,7 +77,6 @@ def reflectedSpoofProtectionONOS():
     requests.post("http://127.0.0.1:8181/onos/v1/flows/",
                   data=json.dumps(pay_load_to_post), auth=('onos', 'rocks'))
 
-
 def generateBlockingRuleONOS(ip, timeout=60, id="of:0000000000000001", isPermanent=False):
     return json.dumps({
         "flows": [{
@@ -105,13 +103,35 @@ def generateBlockingRuleONOS(ip, timeout=60, id="of:0000000000000001", isPermane
         ]
     })
 
+'''
+Function for installing rule and differentiates between ONOS Mode and CLI Mode
+Each thread has its own blist dictionary so shouldn't cause a race condition
+'''
+def installRule(switch, timeout, srcIP, date, blist) :
+    if srcIP not in internalIPs:
+        print("Installing on switch: " + switch)
+        if (not onosMode):
+            os.system("sudo ovs-ofctl del-flows " + switch)
+            blist[srcIP] = date
+            for key in blist:
+                os.system("sudo ovs-ofctl add-flow "+switch+" hard_timeout=" + str(
+                    hardTimeOut) + ",dl_type=0x0800,nw_src=" + str(key) + ",actions=drop")
+            
+            # reflectedSpoofProtection()
+        else:
+            blist[srcIP] = date
+            for key in blist:
+                print("Generate new flow rule addition")
+                x = requests.post("http://127.0.0.1:8181/onos/v1/flows/",
+                                    data=generateBlockingRuleONOS(key, timeout=10), auth=auth)
+            
+
 # TODO : Fix this 
 if (onosMode):
     reflectedSpoofProtectionONOS()
 else:
+    reflectedSpoofProtection()
 
-    # reflectedSpoofProtection()
-    pass
 
 class SuricataConnection (threading.Thread):
     def __init__(self, threadID, name, sock):
@@ -124,8 +144,11 @@ class SuricataConnection (threading.Thread):
         print ("Starting " + self.name)
         print("Reading from sock")
         blist = {}
+        # For anomaly mode
+        dests = {} #{ip : (, )} # {ip : (timestamp, msgsFromSuricata) packet count
+
         while True:
-            line = self.sock.recv(512).decode("utf-8")
+            line = self.sock.recv(475).decode("utf-8")
             try:
                 lines = line.split('\n')
                 for i in lines:
@@ -136,6 +159,9 @@ class SuricataConnection (threading.Thread):
                         print(parsed)
                         if (str(parsed['proto']) == 'ICMP') or (parsed['proto'] == u'TCP' and int(str(parsed['alert']['signature_id'])) == 1):
                             if (not anomalyMode):
+                                ''' 
+                                Just in normal black list mode 
+                                '''
                                 srcIP = parsed['src_ip']
                                 date = datetime.datetime.strptime(
                                     parsed["timestamp"], "%Y-%m-%dT%H:%M:%S.%f%z")
@@ -149,28 +175,40 @@ class SuricataConnection (threading.Thread):
 
                                 switch = suricataInterfaces[parsed['in_iface']]
 
-                                if srcIP not in internalIPs:
-                                    print("Installing on switch: " + switch)
-                                    if (not onosMode):
-                                        os.system("sudo ovs-ofctl del-flows " + switch)
-                                        blist[srcIP] = date
-                                        for key in blist:
-                                            os.system("sudo ovs-ofctl add-flow "+switch+" hard_timeout=" + str(
-                                                hardTimeOut) + ",dl_type=0x0800,nw_src=" + str(key) + ",actions=drop")
-                                                
+                                installRule(switch, hardTimeOut, srcIP, date, blist )
+                                
+                            else: 
+                                '''
+                                Basic anomaly mode
+                                '''
+                                median = 3
+                                stdDev = 1
+
+                                srcIP = parsed['src_ip']
+                                date = datetime.datetime.strptime(
+                                    parsed["timestamp"], "%Y-%m-%dT%H:%M:%S.%f%z")
+
+                                if (srcIP in blist):
+                                    diff = date - blist[srcIP]
+                                    if diff.total_seconds() >= hardTimeOut:
+                                        del blist[srcIP]
                                     else:
-                                        blist[srcIP] = date
-                                        for key in blist:
-                                            print("Generate new flow rule addition")
-                                            x = requests.post("http://127.0.0.1:8181/onos/v1/flows/",
-                                                              data=generateBlockingRuleONOS(key, timeout=10), auth=auth)
+                                        continue
+
+
+                                if srcIP not in dests:
+                                    dests[srcIP] = [date, 1]
+                                else :
+                                    dests[srcIP][1] += 1
+                                
+                                print(dests)
 
                     except Exception as e:
-                        # print(e)
+                        print(e)
                         # print(i)
                         continue
             except Exception as e:
-                # print(e)
+                print(e)
                 # print(i)
                 continue
 
@@ -182,17 +220,15 @@ sock.bind(server_address)
 sock.listen(5)
 
 threads = []
-
-while True:
+for i in range(2):
     conn, addr = sock.accept()
     print("connection accepted...if you havent started suricata\nit means you have some \nsuricata processes still \nleft to kill do this with \n pkill -f suricata")
 
     thread = SuricataConnection(i, "Thread-"+str(i), conn)
-    # Start new Threads
+    # Start new Threads 
     thread.start()
     # Add threads to thread list
     threads.append(thread)
-
 for t in threads:
     t.join()
 
